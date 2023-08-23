e0cadb22baa7bc7eb13a558a2e32f6e8296e82a9
Alexei Guleac <al.guleac@gmail.com>
2023-08-23 09:51:31 +0300

    Add hibernate, spring batch

---
+++ hibernate-xmlconfig/src/main/java/net/example/hibernate/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package net.example.hibernate.dao;
+
+import java.util.List;
+
+import net.example.hibernate.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
